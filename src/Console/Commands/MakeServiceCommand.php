9b95fdb23c23b32ec6b8d01bbf0770161d15ec35
Michael Barrows <mail@michaelbarrows.com>
2022-06-26 18:01:57 +0100

    make service contractable

--- src/Console/Commands/MakeServiceCommand.php
+++ src/Console/Commands/MakeServiceCommand.php
@@ -3,6 +3,7 @@
 namespace MichaelBarrows\ArtisanMakeCommands\Console\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputOption;
 
 class MakeServiceCommand extends GeneratorCommand
 {
@@ -18,5 +19,26 @@ class MakeServiceCommand extends GeneratorCommand
     protected function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace.'\Services';
+    }
+
+    protected function getOptions()
+    {
+        return [
+            ['contract', null, InputOption::VALUE_NONE, 'Specify the contract to bind the service to.'],
+        ];
+    }
+
+    protected function buildClass($name)
+    {
+        $replace = [];
+
+        if ($this->option('contract')) {
+            $replace['{{ contract }}'] = $this->option->contract;
+            $replace['{{ contractImport }}'] = "use \\App\\Contracts\\{$this->option->contract};";
+        }
+
+        $replace['{{ service }}'] = $name;
+
+        return $replace;
     }
 }
