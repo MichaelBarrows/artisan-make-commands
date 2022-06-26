d14493c22c289220526b567be011392baae90bec
Michael Barrows <mail@michaelbarrows.com>
2022-06-26 16:55:05 +0100

    register the command

--- src/Providers/CommandsServiceProvider.php
+++ src/Providers/CommandsServiceProvider.php
@@ -3,12 +3,15 @@
 namespace MichaelBarrows\ArtisanMakeCommands\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use MichaelBarrows\ArtisanMakeCommands\Console\Commands;
 
 class CommandsServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->commands([
+            Commands\MakeActionCommand::class,
+        ]);
     }
 
     public function boot()
