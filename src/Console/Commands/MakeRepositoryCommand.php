6de31f30fc59849d12fb39db89b88b84dfdd80fa
Michael Barrows <m+github@mbws.uk>
2024-08-11 16:50:43 +0100

    tidy up

--- src/Console/Commands/MakeRepositoryCommand.php
+++ src/Console/Commands/MakeRepositoryCommand.php
@@ -4,7 +4,6 @@ namespace MichaelBarrows\ArtisanMakeCommands\Console\Commands;
 
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
-use Symfony\Component\Console\Input\InputOption;
 
 class MakeRepositoryCommand extends GeneratorCommand
 {
